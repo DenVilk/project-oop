a4e248859d9eed0b40ea6e324b45076c0e798942
Garik77770 <99966350+Garik77770@users.noreply.github.com>
2022-03-29 11:54:28 +0300

    projekt

---
+++ main.py
@@ -0,0 +1,8 @@
+class Workshop:
+    def __init__(self, receipt, product_type, Acceptance_date, date_of_completion, FIO, Status):
+        self.receipt = receipt
+        self.product_type = product_type
+        self.Acceptance_date = Acceptance_date
+        self.date_of_completion = date_of_completion
+        self.FIO = FIO
+        self.Status = Status
